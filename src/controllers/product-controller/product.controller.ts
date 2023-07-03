d42a11b485474ce5174469a202a16ffe286fbfdb
Von Aralar <von.aralar@gmail.com>
2023-07-03 16:54:27 +0800

    Product(list): Added list query

--- src/controllers/product-controller/product.controller.ts
+++ src/controllers/product-controller/product.controller.ts
@@ -2,21 +2,102 @@ import logger from "@utils/logger";
 import { Request, Response, NextFunction } from "express";
 import { JwtPayload } from "jsonwebtoken";
 import db from "@models/index";
+import mongoose from "mongoose";
 import { seedProducts } from "@src/helper/product-db";
 const Product = db.productdb;
 
 interface AuthenticatedRequest extends Request {
   user?: JwtPayload;
 }
+interface ProductResponse {
+  product: any; // Update the type of the product object with your product schema
+  cursor: string;
+}
+
+interface PageInfo {
+  startCursor: string;
+  endCursor: string;
+  hasNextPage: boolean;
+  hasPreviousPage: boolean;
+  totalCount: number;
+}
+
+interface ListProductResponse {
+  products: ProductResponse[];
+  pageInfo: PageInfo;
+}
+
 export const ListProduct = async (
   req: AuthenticatedRequest,
   res: Response,
   next: NextFunction
 ): Promise<void> => {
   try {
+    const { first, last, after, before, order } = req.query;
+    const user = req.user;
+    const sort: string = req.query.sort?.toString() || "";
+
+    // Create the filter based on the cursor and sort parameters
+    const filter: any = {};
+    if (after) {
+      filter._id = { $gt: after };
+    } else if (before) {
+      filter._id = { $lt: before };
+    }
+
+    // Fetch the products from the database with pagination and sorting
+    const query = Product.find(filter)
+      .sort({ [sort]: order === "desc" ? -1 : 1 })
+      .limit(Number(first || last));
+
+    const products = await query.exec();
+
+    // Generate cursors for pagination
+    const startCursor = products.length > 0 ? products[0]._id.toString() : "";
+    const endCursor =
+      products.length > 0 ? products[products.length - 1]._id.toString() : "";
+
+    const hasNextPage = !!after || products.length > Number(first || last);
+    const hasPreviousPage = !!before;
+
+    const totalCount = await Product.countDocuments();
+
+    const productResponse: ProductResponse[] = products.map((product) => ({
+      product,
+      cursor: product._id.toString(),
+    }));
+
+    const pageInfo: PageInfo = {
+      startCursor,
+      endCursor,
+      hasNextPage,
+      hasPreviousPage,
+      totalCount,
+    };
+
+    const response: ListProductResponse = {
+      products: productResponse,
+      pageInfo,
+    };
+
+    res.json(response);
+  } catch (error: unknown) {
+    logger.error("Error in ListProduct:", error);
+    next(error);
+  }
+};
+export const ListProduct2 = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
     // Extract the query parameters
-    const { before, order } = req.query;
+    const { order } = req.query;
     const after: string = req.query.after?.toString() || "";
+    const before: string = req.query.before?.toString() || "";
+    console.log(before);
+
     const first: string = req.query.first?.toString() || "";
     const last: string = req.query.last?.toString() || "";
     const sort: string = req.query.sort?.toString() || "";
@@ -24,21 +105,20 @@ export const ListProduct = async (
     // Build the pagination options
     const paginationOptions: any = {};
 
-    // if (first) {
-    //   paginationOptions.limit = parseInt(first, 10);
-    // }
+    if (first) {
+      paginationOptions.limit = parseInt(first, 10);
+    }
 
-    // if (last) {
-    //   paginationOptions.limit = parseInt(last, 10);
-    // }
+    if (last) {
+      paginationOptions.limit = parseInt(last, 10);
+    }
 
     if (after) {
-      console.log(after);
-      paginationOptions.startAfter = { price_id: after };
+      paginationOptions.startAfter = new mongoose.Types.ObjectId(after);
     }
 
     if (before) {
-      paginationOptions.startBefore = { price_id: before };
+      paginationOptions.startBefore = new mongoose.Types.ObjectId(before);
     }
 
     // Build the sorting options
@@ -47,24 +127,28 @@ export const ListProduct = async (
     if (sort && order) {
       sortOptions[sort] = order === "desc" ? -1 : 1;
     }
-
+    console.log(before);
+    console.log(paginationOptions.startBefore);
     // Query the products
     const products = await Product.find({}, null, paginationOptions).sort(
       sortOptions
     );
 
+    // Determine the values of hasNextPage and hasPreviousPage
+    const hasNextPage = products.length === paginationOptions.limit;
+    const hasPreviousPage = paginationOptions.startBefore !== undefined;
+
     // Create the response object
     const response = {
       products: products.map((product) => ({
         product,
-        // product_ids: product._id,
-        cursor: generateOpaqueCursor(product._id),
+        cursor: generateOpaqueCursor(product.id),
       })),
       pageInfo: {
-        startCursor: generateOpaqueCursor(products[0]?._id),
-        endCursor: generateOpaqueCursor(products[products.length - 1]?._id),
-        hasNextPage: false, // Modify this based on your pagination logic
-        hasPreviousPage: false, // Modify this based on your pagination logic
+        startCursor: generateOpaqueCursor(products[0]?.id),
+        endCursor: generateOpaqueCursor(products[products.length - 1]?.id),
+        hasNextPage,
+        hasPreviousPage,
         totalCount: products.length,
       },
     };
@@ -75,7 +159,6 @@ export const ListProduct = async (
     next(error);
   }
 };
-
 // Helper function to generate an opaque cursor
 const generateOpaqueCursor = (id: string): string => {
   // Logic to generate an opaque cursor based on the ID
